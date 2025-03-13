db36a605ae0283e359ad8d0d28f119a88ee9b5dd
lakihaomal <lhw150030@utdallas.edu>
2025-03-13 15:28:49 -0500

    minor updates

--- firmware/xu4Mqtt/chamberTest.py
+++ firmware/xu4Mqtt/chamberTest.py
@@ -3,13 +3,6 @@ import time
 import sys
 from datetime import datetime
 import yaml
-# Create a class named chamber where it initially checks 
-# the availabilty of the chamber and then creates an object 
-# with the input values the available chamber will give 
-# Initially read how many chambers and then create an 
-# object with the chamber index as an input if available 
-# Have 2 sensor IDs one with variable values and one with 
-# default constants 
 
 import json, requests
 import mintsXU4.especChamber.chamber as eC
@@ -31,120 +24,9 @@ if chamber_index is None:
 
 # Create the chamber object only if an index is available
 chamber = Chamber(url, chamber_index, token)
-# data = chamber.execute_command("")
-# print(json.dumps(data, indent=4))
 
 chamber.stop_chamber()
 
-# API details
+chamber.change_temperature(50)
 
-# print("TEST 1")
-# url = "http://192.168.20.113/api/v4/chambers/1/constants/constant_1"
-# headers = {
-#     "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzg4MjMxNTMsIm5iZiI6MTczODgyMzE1MywianRpIjoiYWZmOGE5ODYtOTc0Ny00MGQ0LWFjYjgtYzI0NGY4NzU3ZmJmIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.zWMTeiVwyahVfqFhIva2z5-I00PwGBFBnj4YONDvQgo",
-#     "Content-Type": "application/json"
-# }
-# data = {
-#     "temp": {"group": "loop", "set_value": 25, "range": [-20, 180]},
-#     "humi": {"group": "loop", "enable": True, "set_value": 43, "range": [10, 95]},
-#     "time_signal_1": {"group": "output", "value": True},
-#     "dap": {"group": "output", "value": False}
-# }
-# try:
-#     response = requests.post(url, headers=headers, json=data)
-#     print(response)
-#     response.raise_for_status()
-#     print("Response:", response.json())
-# except requests.exceptions.HTTPError as err:
-#     print("HTTP Error:", err)
-# except Exception as e:
-#     print("Error:", e)
-
-
-# GETS CURRENT TEMPERATURE SETTINGS - EVEN THE ONES THAT I CANT CONTROL = Enabled, Power  - These I save at Initialization - Better to loop it too
-# curl -X GET \
-#      -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzk4NTE4OTMsIm5iZiI6MTczOTg1MTg5MywianRpIjoiNmY1MmZlNTItYTljYi00NzA2LTg1YmQtNzg1ZWFiNGYxZjJiIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.eRfrrVUtq9BD0tZH7nwzhsHHNQ0i479Tu668B_pmf2E" \
-#      http://192.168.20.113/api/v4/chambers/1/conditions/temp
-# {
-#     "temp": {
-#         "group": "loop",
-#         "enable": false,
-#         "set_value": 25.0,
-#         "process_value": 24.96561622619629,
-#         "power": 0.0,
-#         "units": "\u00b0C"
-#     },
-#     "_errors": []
-# }%    
-
-
-# GETS CURRENT HUMIDITY SETTINGS - EVEN THE ONES THAT I CANT CONTROL = Enabled, Power - Better to loop it too
-# curl -X GET \
-#      -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzk4NTE4OTMsIm5iZiI6MTczOTg1MTg5MywianRpIjoiNmY1MmZlNTItYTljYi00NzA2LTg1YmQtNzg1ZWFiNGYxZjJiIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.eRfrrVUtq9BD0tZH7nwzhsHHNQ0i479Tu668B_pmf2E" \
-#      http://192.168.20.113/api/v4/chambers/1/conditions/humi
-# {
-#     "humi": {
-#         "group": "loop",
-#         "enable": false,
-#         "set_value": 43.0,
-#         "process_value": 42.01858901977539,
-#         "power": 0.0,
-#         "units": "%RH"
-#     },
-#     "_errors": []
-# }%          
-
-
-
-# STOPS WHAT EVER CURRENT OPERATION 
-# curl -X POST \
-#      -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzk4NTE4OTMsIm5iZiI6MTczOTg1MTg5MywianRpIjoiNmY1MmZlNTItYTljYi00NzA2LTg1YmQtNzg1ZWFiNGYxZjJiIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.eRfrrVUtq9BD0tZH7nwzhsHHNQ0i479Tu668B_pmf2E" \
-#      -H "Content-Type: application/json;charset=utf-8" \
-#      -d '{"group":"standby"}' \
-#      http://192.168.20.113/api/v4/chambers/1/operations/stop_chamber
-
-
-# RUN  Constant - Everything must be set 
-# curl -X POST \
-#      -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzk4NTE4OTMsIm5iZiI6MTczOTg1MTg5MywianRpIjoiNmY1MmZlNTItYTljYi00NzA2LTg1YmQtNzg1ZWFiNGYxZjJiIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.eRfrrVUtq9BD0tZH7nwzhsHHNQ0i479Tu668B_pmf2E" \
-#      -H "Content-Type: application/json;charset=utf-8" \
-#      -d '{"group":"constant"}' \
-#      http://192.168.20.113/api/v4/chambers/1/operations/start_constant
-
-
-
-# Only one constant mode available - so both these are identical - I would just update the second one 
-# Constant Modes List
-# Retrieve a list of available constant mode settings.
-# request will update the running constant mode.
-# curl -X POST \
-#      -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzk4NTE4OTMsIm5iZiI6MTczOTg1MTg5MywianRpIjoiNmY1MmZlNTItYTljYi00NzA2LTg1YmQtNzg1ZWFiNGYxZjJiIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.eRfrrVUtq9BD0tZH7nwzhsHHNQ0i479Tu668B_pmf2E" \
-#      -H "Content-Type: application/json;charset=utf-8" \
-#      -d '{"temp":{"group":"loop","set_value":23,"range":[-20,180]},"humi":{"group":"loop","enable":true,"set_value":42,"range":[10,95]},"time_signal_1":{"group":"output","value":true},"dap":{"group":"output","value":false}}' \
-#      http://192.168.20.113/api/v4/chambers/1/constants
-
-
-# Constant Mode
-# Get the constant mode for a specified mode.
-# curl -X POST \
-#      -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzk4NTE4OTMsIm5iZiI6MTczOTg1MTg5MywianRpIjoiNmY1MmZlNTItYTljYi00NzA2LTg1YmQtNzg1ZWFiNGYxZjJiIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.eRfrrVUtq9BD0tZH7nwzhsHHNQ0i479Tu668B_pmf2E" \
-#      -H "Content-Type: application/json;charset=utf-8" \
-#      -d '{"temp":{"group":"loop","set_value":22,"range":[-20,180]},"humi":{"group":"loop","enable":true,"set_value":44,"range":[10,95]},"time_signal_1":{"group":"output","value":true},"dap":{"group":"output","value":false}}' \
-#      http://192.168.20.113/api/v4/chambers/1/constants/constant_1
-
-# Run Operation
-# Operations: Run Constant Mode
-# Issue the command "Run Constant Mode" to the chamber.
-# curl -X POST \
-#      -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzk4NTE4OTMsIm5iZiI6MTczOTg1MTg5MywianRpIjoiNmY1MmZlNTItYTljYi00NzA2LTg1YmQtNzg1ZWFiNGYxZjJiIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.eRfrrVUtq9BD0tZH7nwzhsHHNQ0i479Tu668B_pmf2E" \
-#      -H "Content-Type: application/json;charset=utf-8" \
-#      -d '{"group":"constant"}' \
-#      http://192.168.20.113/api/v4/chambers/1/operations/start_constant
-
-# Operations: Stop Operation
-# Issue the command "Stop Operation" to the chamber.
-# curl -X POST \
-#      -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzk4NTE4OTMsIm5iZiI6MTczOTg1MTg5MywianRpIjoiNmY1MmZlNTItYTljYi00NzA2LTg1YmQtNzg1ZWFiNGYxZjJiIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.eRfrrVUtq9BD0tZH7nwzhsHHNQ0i479Tu668B_pmf2E" \
-#      -H "Content-Type: application/json;charset=utf-8" \
-#      -d '{"group":"standby"}' \
-#      http://192.168.20.113/api/v4/chambers/1/operations/stop_chamber
+chamber.change_humdity(50)
--- firmware/xu4Mqtt/mintsXU4/especChamber/chamber.py
+++ firmware/xu4Mqtt/mintsXU4/especChamber/chamber.py
@@ -56,6 +56,7 @@ def get_available_chamber_index(url, token, chamberWanted=0, timeout=5):
     return None
 
 class Chamber:
+
     def __init__(self, url, chamber_index, token):
         """
         Initializes a Chamber instance.
@@ -86,7 +87,14 @@ class Chamber:
             40: "Operation Run",
         }
 
+        self.get_and_write_summary(expanded=True)
 
+        print("Chamber Initiated with IP:"\
+               + self.url + " at chamber index:" \
+                + str(chamber_index))
+
+
+    def get_and_write_summary(self, expanded=False):
        # Execute command and retrieve data
         self.validity, data = self.execute_command("")
         print(data)
@@ -105,14 +113,89 @@ class Chamber:
             self.write_status(dateTime,status)
             self.write_temperature_set(dateTime,temperature_set)
             self.write_humidity_set(dateTime,humidity_set)
-            self.write_time_signal_set(dateTime,time_signal_set)
-            self.write_dap_set(dateTime,dap_set)
-            self.write_program_set(dateTime,program_set)
+            if expanded:
+                self.write_time_signal_set(dateTime,time_signal_set)
+                self.write_dap_set(dateTime,dap_set)
+                self.write_program_set(dateTime,program_set)
         
         else:
             print("Chamber data invalid")
 
-              
+
+    def  write_status(self,dateTime,status):
+            sensorIDPost            = "STATUS"
+            self.chamber_time       = status["datetime"]
+            self.operationLabel     = status["operation"]
+            self.operation          = self.operation_mapping.get(self.operationLabel,-1)
+            self.constant_key       = status["constant_key"]
+            self.program_step       = self.default_if_none(status["program_step"])
+            self.program_time_step  = self.default_if_empty(status['program_time_step'])
+            self.program_time       = self.default_if_empty(status["program_time"])
+            self.program_key        = self.default_if_none(status["program_key"])
+            self.program_name       = status["program_name"]
+            self.program            = self.default_if_none(status["program"])
+            self.program_counters   = ", ".join(status["program_counters"])
+            self.running            = self.bool_to_int(status["running"])
+
+            sensorDictionary = OrderedDict([
+                ("dateTime"             ,str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
+                ("hostIP"               ,str(self.url)),
+                ("chamberTime"          ,str(self.chamber_time)),                
+                ("operation"            ,str(self.operation)),          
+                ("operationLabel"       ,str(self.operationLabel)),             
+                ("programStep"          ,int(self.program_step)),          
+                ("programTime"          ,int(self.program_time)),       
+                ("programKey"           ,int(self.program_key)),       
+                ("programName"          ,str(self.program_name)),       
+                ("program"              ,str(self.program)),    
+                ("program_counters"     ,str(self.program_counters)),    
+                ("running"              ,int(self.running)),    
+                ])
+
+            mSR.sensorFinisher(dateTime,"BTL433ESC001" + sensorIDPost ,sensorDictionary)
+
+
+
+    def  write_temperature_set(self,dateTime,status):
+            sensorIDPost                        = "TMPRT"
+            self.temperature_group              = status["group"]
+            self.temperature_enable             = self.bool_to_int(status["enable"])
+            self.temperature_set_value          = status["set_value"]
+            self.temperature_process_value      = status["process_value"]
+            self.temperature_power              = status["power"]
+
+            sensorDictionary = OrderedDict([
+                ("dateTime"         ,str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
+                ("temperatureGroup" ,str(self.temperature_group)),                
+                ("enabled"          ,int(self.temperature_enable)),             
+                ("setValue"         ,self.temperature_set_value),          
+                ("processValue"     ,self.temperature_process_value),       
+                ("power"            ,self.temperature_power),       
+                ])
+            mSR.sensorFinisher(dateTime,"BTL433ESC001" + sensorIDPost ,sensorDictionary)
+
+
+
+    def  write_humidity_set(self,dateTime,status):
+            sensorIDPost                     = "HMDTY"
+            self.humidity_group              = status["group"]
+            self.humidity_enable             = self.bool_to_int(status["enable"])
+            self.humidity_set_value          = status["set_value"]
+            self.humidity_process_value      = status["process_value"]
+            self.humidity_power              = status["power"]
+
+            sensorDictionary = OrderedDict([
+                ("dateTime"         ,str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
+                ("humidityGroup"    ,str(self.humidity_group)),                
+                ("enabled"          ,int(self.humidity_enable)),             
+                ("setValue"         ,self.humidity_set_value),          
+                ("processValue"     ,self.humidity_process_value),       
+                ("power"            ,self.humidity_power),       
+                ])
+            mSR.sensorFinisher(dateTime,"BTL433ESC001" + sensorIDPost ,sensorDictionary)
+
+
+
     def  write_program_set(self,dateTime,status):
             sensorIDPost                = "PRGRM"
             self.program_name_1         = status["1"]["name"]
@@ -153,75 +236,6 @@ class Chamber:
                 ("timeSignalGroup"  ,str(self.time_signal_group)),                
                 ("enabled"          ,int(self.time_signal_enabled)),             
                 ])
-            mSR.sensorFinisher(dateTime,"BTL433ESC001" + sensorIDPost ,sensorDictionary)
-
-    def  write_humidity_set(self,dateTime,status):
-            sensorIDPost                     = "HMDTY"
-            self.humidity_group              = status["group"]
-            self.humidity_enable             = self.bool_to_int(status["enable"])
-            self.humidity_set_value          = status["set_value"]
-            self.humidity_process_value      = status["process_value"]
-            self.humidity_power              = status["power"]
-
-            sensorDictionary = OrderedDict([
-                ("dateTime"         ,str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
-                ("humidityGroup"    ,str(self.humidity_group)),                
-                ("enabled"          ,int(self.humidity_enable)),             
-                ("setValue"         ,self.humidity_set_value),          
-                ("processValue"     ,self.humidity_process_value),       
-                ("power"            ,self.humidity_power),       
-                ])
-            mSR.sensorFinisher(dateTime,"BTL433ESC001" + sensorIDPost ,sensorDictionary)
-
-    def  write_temperature_set(self,dateTime,status):
-            sensorIDPost                        = "TMPRT"
-            self.temperature_group              = status["group"]
-            self.temperature_enable             = self.bool_to_int(status["enable"])
-            self.temperature_set_value          = status["set_value"]
-            self.temperature_process_value      = status["process_value"]
-            self.temperature_power              = status["power"]
-
-            sensorDictionary = OrderedDict([
-                ("dateTime"         ,str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
-                ("temperatureGroup" ,str(self.temperature_group)),                
-                ("enabled"          ,int(self.temperature_enable)),             
-                ("setValue"         ,self.temperature_set_value),          
-                ("processValue"     ,self.temperature_process_value),       
-                ("power"            ,self.temperature_power),       
-                ])
-            mSR.sensorFinisher(dateTime,"BTL433ESC001" + sensorIDPost ,sensorDictionary)
-
-
-    def  write_status(self,dateTime,status):
-            sensorIDPost            = "STATUS"
-            self.chamber_time       = status["datetime"]
-            self.operationLabel     = status["operation"]
-            self.operation          = self.operation_mapping.get(self.operationLabel,-1)
-            self.constant_key       = status["constant_key"]
-            self.program_step       = self.default_if_none(status["program_step"])
-            self.program_time_step  = self.default_if_empty(status['program_time_step'])
-            self.program_time       = self.default_if_empty(status["program_time"])
-            self.program_key        = self.default_if_none(status["program_key"])
-            self.program_name       = status["program_name"]
-            self.program            = self.default_if_none(status["program"])
-            self.program_counters   = ", ".join(status["program_counters"])
-            self.running            = self.bool_to_int(status["running"])
-
-            sensorDictionary = OrderedDict([
-                ("dateTime"             ,str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
-                ("hostIP"               ,str(self.url)),
-                ("chamberTime"          ,str(self.chamber_time)),                
-                ("operation"            ,str(self.operation)),          
-                ("operationLabel"       ,str(self.operationLabel)),             
-                ("programStep"          ,int(self.program_step)),          
-                ("programTime"          ,int(self.program_time)),       
-                ("programKey"           ,int(self.program_key)),       
-                ("programName"          ,str(self.program_name)),       
-                ("program"              ,str(self.program)),    
-                ("program_counters"     ,str(self.program_counters)),    
-                ("running"              ,int(self.running)),    
-                ])
-
             mSR.sensorFinisher(dateTime,"BTL433ESC001" + sensorIDPost ,sensorDictionary)
 
     def bool_to_int(self,value):
@@ -271,9 +285,8 @@ class Chamber:
         success, response = self.execute_command(\
              "operations/stop_chamber",\
                   data=data, method="POST")
-        
-        print(response)
-
+        pprint.pprint(response)
+        time.sleep(1)
 
         sensorIDPost            = "CHNG"
         self.change_ID           = 1
@@ -283,14 +296,145 @@ class Chamber:
         sensorDictionary = OrderedDict([
             ("dateTime"             ,str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
             ("changeID"             ,self.change_ID),
-            ("changeLabel"          ,self.change_mapping.get(self.change_ID)),                
+            ("changeLabel"          ,self.change_mapping.get(self.change_ID)),
+            ("newValue"             ,-100),   
             ("success"              ,self.bool_to_int(success)),
             ])
         mSR.sensorFinisher(dateTime,"BTL433ESC001" + sensorIDPost ,sensorDictionary)
+        self.get_and_write_summary()
 
-# change_temperature
-# change_humidity
-# set_constant (inputs should have setTemperature True or false)
+
+
+        return success
+
+
+    def set_temperature_value(self, temp_value):
+        """
+        Updates the temperature set value.
+
+        :param temp_value: The new temperature set value.
+        """
+        data = {
+            "temp": {
+                "group": "loop",
+                "set_value": temp_value,
+                "range": [-20, 180]
+            }
+        }
+
+        success, response          = self.execute_command("constants/constant_1", data=data, method="POST")
+        pprint.pprint(response)
+        time.sleep(1)
+
+        dateTime          = datetime.now(timezone.utc)
+        sensorIDPost      = "CHNG"
+
+        self.change_ID    = 10  # Change ID for Temperature Set
+
+        # Log the change
+        sensorDictionary = OrderedDict([
+            ("dateTime"     , str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
+            ("changeID"     , self.change_ID),
+            ("changeLabel"  , self.change_mapping.get(self.change_ID)),
+            ("newValue"     , temp_value),
+            ("success"      , self.bool_to_int(success)),
+        ])
+        mSR.sensorFinisher(dateTime, "BTL433ESC001" + sensorIDPost, sensorDictionary)
+
+        return success
+
+
+    def set_humidity_value(self, humi_value):
+        """
+        Updates the humidity set value.
+
+        :param humi_value: The new humidity set value.
+        """
+        data = {
+            "humi": {
+                "group": "loop",
+                "enable": True,
+                "set_value": humi_value,
+                "range": [10, 95]  # Assuming valid humidity range
+            }
+        }
+
+        success, response = self.execute_command("constants/constant_1", data=data, method="POST")
+        pprint.pprint(response)
+        time.sleep(1)
+
+        dateTime = datetime.now(timezone.utc)
+        sensorIDPost = "CHNG"
+        self.change_ID = 11  # Change ID for Humidity Set
+
+        # Log the change
+        sensorDictionary = OrderedDict([
+            ("dateTime"     , str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
+            ("changeID"     , self.change_ID),
+            ("changeLabel"  , self.change_mapping.get(self.change_ID)),
+            ("newValue"     , humi_value),
+            ("success"      , self.bool_to_int(success)),
+        ])
+        mSR.sensorFinisher(dateTime, "BTL433ESC001" + sensorIDPost, sensorDictionary)
+
+        return success
+    
+    def start_constant_mode(self):
+        """
+        Starts the chamber in Constant Mode.
+
+        :return: Success status (True/False)
+        """
+        data = {"group": "constant"}
+
+        success, response = self.execute_command("operations/start_constant", data=data, method="POST")
+        pprint.pprint(response)
+        time.sleep(1)
+
+        dateTime       = datetime.now(timezone.utc)
+        sensorIDPost   = "CHNG"
+        self.change_ID = 20  # Change ID for Constant Mode Start
+
+        # Log the change
+        sensorDictionary = OrderedDict([
+            ("dateTime", str(dateTime.strftime('%Y-%m-%d %H:%M:%S.%f'))),
+            ("changeID"     , self.change_ID),
+            ("changeLabel"  , self.change_mapping.get(self.change_ID)),
+            ("newValue"     , -100),
+            ("success"      , self.bool_to_int(success)),
+            ])
+
+        mSR.sensorFinisher(dateTime, "BTL433ESC001" + sensorIDPost, sensorDictionary)
+
+        return success
+
+
+    def change_temperature(self,temp_value):
+        self.set_temperature_value( temp_value)
+        self.start_constant_mode()
+        self.get_and_write_summary()
+
+
+    def change_humdity(self,humi_value):
+        self.set_humidity_value(humi_value)
+        self.start_constant_mode()
+        self.get_and_write_summary()
+
+
+
+# set_temperature
+
+
+# set_humidity  
 # activate_constant_mode
 # write_change t
+
+
+# Constant Mode
+# Get the constant mode for a specified mode.
+# curl -X POST \
+#      -H "Authorization: Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpYXQiOjE3Mzk4NTE4OTMsIm5iZiI6MTczOTg1MTg5MywianRpIjoiNmY1MmZlNTItYTljYi00NzA2LTg1YmQtNzg1ZWFiNGYxZjJiIiwiaWRlbnRpdHkiOiJhZG1pbiIsImZyZXNoIjpmYWxzZSwidHlwZSI6ImFjY2VzcyIsInVzZXJfY2xhaW1zIjp7InJvbGVzIjpbImNvbmRpdGlvbnNfcnciLCJvcGVyYXRpb25zX3J3IiwiZmVhdHVyZXNfcnciLCJjb25zdGFudHNfcnciLCJwcm9ncmFtc19ydyIsInNldHVwX3J3Il19fQ.eRfrrVUtq9BD0tZH7nwzhsHHNQ0i479Tu668B_pmf2E" \
+#      -H "Content-Type: application/json;charset=utf-8" \
+#      -d '{"temp":{"group":"loop","set_value":22,"range":[-20,180]},"humi":{"group":"loop","enable":true,"set_value":44,"range":[10,95]},"time_signal_1":{"group":"output","value":true},"dap":{"group":"output","value":false}}' \
+#      http://192.168.20.113/api/v4/chambers/1/constants/constant_1
 
