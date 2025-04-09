4e885a74e162dcbb291f6469b8bf7f9af436bfc9
lakihaomal <lhw150030@utdallas.edu>
2025-04-09 15:57:03 -0500

    minor updates

--- firmware/xu4Mqtt/status.sh
+++ firmware/xu4Mqtt/status.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 #
 sleep 1
-echo "IPS7100"
-echo $(pgrep -f 'ips7100ReaderV1.py')
+echo "Routine status check"
+echo "===================="
+echo $(pgrep -f 'runRoutine.py')
 sleep 2
 
-echo "i2c"
-echo  $(pgrep -f 'i2cReader.py')
-sleep 2
-
-echo "GPS"
-echo $(pgrep -f 'gpsReader.py')
-sleep 2
-
-echo "battery reader"
-echo $(pgrep -f 'batteryReader.py')
-sleep 2
